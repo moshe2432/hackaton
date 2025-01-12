fa7df9941d5b0c58e7d51a853170d6922d2b0a59
moshe2432 <moshekl@post.bgu.ac.il>
2025-01-12 12:01:49 +0200

    server brodcast

--- client.py
+++ client.py
@@ -1,4 +1,22 @@
-import socket
+from socket import *
 import time
 
 def main():
+    # Create a socket object
+    tcp_socket = socket(AF_INET, SOCK_STREAM)
+    print("Socket created")
+
+    # Define the port on which you want to connect
+    port = 12345
+    Host_ip = input("Enter the IP address of the server: ")
+
+    tcp_socket.connect((Host_ip, port))
+    print("socket connected to %s" % (port))
+    while True:
+        message = input("Enter message: ")
+        tcp_socket.sendall(message.encode())
+        data = tcp_socket.recv(1024)
+        print("Client received", repr(data))
+        time.sleep(1)
+        if not data:
+            break
--- server.py
+++ server.py
@@ -1,12 +1,49 @@
-import socket
+from socket import *
+import time
+import _thread
 
 def main():
+
     # Create a socket object
-    s = socket.socket()
+    tcp_socet = socket(AF_INET, SOCK_STREAM)
     print("Socket created")
 
     # Define the port on which you want to connect
     port = 12345
-    ipAddr = '172.1.0.4'
-    # connect to the server on local computer
-    s.listen(5)
+    Host_ip = '172.0.0.1'
+    
+
+    tcp_socet.bind((Host_ip, port))
+    print("socket binded to %s" % (port))
+    tcp_socet.listen()
+    print("socket is listening")
+    
+    conn, addr = tcp_socet.accept()
+
+    print("Got connection from", addr)
+    while True:
+        data = conn.recv(1024)
+        print("Server received", repr(data))
+        reply = input("Reply: ")
+        conn.sendall(reply.encode())
+        if not data:
+            break
+
+
+def UDP_Brodcast():
+    # Create a socket object
+    udp_socket = socket(AF_INET, SOCK_DGRAM)
+    print("Socket created")
+    mesege = "Server started, listening on IP address ??.??.??.??"
+    # Define the port on which you want to connect
+    port = 12345
+
+    udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    while True:
+        udp_socket.sendto(mesege.encode(), ('<broadcast>', port))
+        time.sleep(1)
+
+
+
+if __name__ == "__main__":
+    main()
