fb613cb6c2793325f5c201298be09edfb131e623
moshe2432 <moshekl@post.bgu.ac.il>
2025-01-12 20:37:27 +0200

    server udp protocol

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from socket import *
 import time
 import _thread
+import struct
+
+packet_size = 1024
 
 def main():
 
@@ -30,17 +33,49 @@ def main():
             break
 
 
+def UDP_Payload(addres,size,file):
+    socket = socket(AF_INET, SOCK_DGRAM)
+    file = open(file, "rb")
+    index = 0
+    while size:
+        if size > packet_size:
+            data = file.read(packet_size)
+            size -= packet_size
+        else:
+            data = file.read(size)
+            size = 0
+        index+=1
+
+        """
+        21 bit header:
+        magic number: 0xabcddcba
+        message type: 0x04
+        Total segment count: unsigned int, size/packet_size
+        Packet index: unsigned int, index
+        """
+        header = struct.pack(0xabcddcba,0x04,size//packet_size + 1 ,index)  
+        packet = header + data
+        socket.sendto(packet.encode(), addres)
+
+
 def UDP_Brodcast():
     # Create a socket object
     udp_socket = socket(AF_INET, SOCK_DGRAM)
     print("Socket created")
-    mesege = "Server started, listening on IP address ??.??.??.??"
+    """
+    9 bit messege:
+    magic number: 0xabcddcba
+    message type: 0x02
+    UDP port: 0x303A
+    TCP port: 0x303B
+    """
+    packet = struct.pack(0xabcddcba,0x02,)  # Packet index as unsigned int
     # Define the port on which you want to connect
     port = 12345
 
     udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     while True:
-        udp_socket.sendto(mesege.encode(), ('<broadcast>', port))
+        udp_socket.sendto(packet.encode(), ('<broadcast>', port))
         time.sleep(1)
 
 
