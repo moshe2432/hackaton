6be1aab650c32f231e4a06131f3e4144fa5f3c79
moshe2432 <moshekl@post.bgu.ac.il>
2025-01-12 20:54:22 +0200

    client udp request

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from socket import *
 import time
+import struct
 
 def main():
     # Create a socket object
@@ -20,3 +21,46 @@ def main():
         time.sleep(1)
         if not data:
             break
+
+def UDP_Request(addres,size):
+    socket = socket(AF_INET, SOCK_DGRAM)
+    packet = struct.pack(0xabcddcba,0x03,size)
+    socket.sendto(packet.encode(), addres)
+    packet_resived = []
+    #start timer
+    start = time.time()
+    #receive the packets
+    while True:
+        data = socket.recv(1045)
+        if not data:
+            break
+        #parse the header
+        header = struct.unpack(data[:21])
+        #check if the packet is correct
+        if header[0] != 0xabcddcba or header[1] != 0x04:
+            continue
+        packet_resived.append(header[3])
+        #check if the packet is the last one
+        if header[3] == header[2]:
+            break
+    
+    #end timer
+    end = time.time()
+
+    #calculate and print the speed
+    print(f"UDP transfer {x} finished, total time: {end-start} seconds. \ntotal speed: {size/(end-start)} bits/second.\npercentage of packets received successfully: {len(packet_resived)*100/header[2]}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
